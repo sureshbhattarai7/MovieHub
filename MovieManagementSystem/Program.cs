527c0fa99013f9acfa82fd801a324c531a5a8dac
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-02-15 17:55:12 +0545

    Add Db Context Service

--- MovieManagementSystem/Program.cs
+++ MovieManagementSystem/Program.cs
@@ -1,7 +1,11 @@
+using MovieManagementSystem.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>();
 
 var app = builder.Build();
 
