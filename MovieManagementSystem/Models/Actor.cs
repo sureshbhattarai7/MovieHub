464beac016c5a4dac78d8a40bbea11034954e35d
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-02-19 22:51:37 +0545

    Modified Actor Model

--- MovieManagementSystem/Models/Actor.cs
+++ MovieManagementSystem/Models/Actor.cs
@@ -23,15 +23,5 @@ namespace MovieManagementSystem.Models
         //Relationship
         public List<Actor_Movie>? Actors_Movies { get; set; }
 
-        //Cinema
-        public int CinemaId { get; set; }
-        [ForeignKey("CinemaId")]
-        public Cinema? Cinema { get; set; }
-
-        //Producer
-        public int ProducerId { get; set; }
-        [ForeignKey("ProducerId")]
-        public Producer? Producer { get; set; }
-
     }
 }
