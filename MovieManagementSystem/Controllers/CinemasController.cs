e637f2d65f363d55bf1450e6a1315fccbba18171
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-02-20 20:48:08 +0545

    Add Index Controller of Cinema

---
+++ MovieManagementSystem/Controllers/CinemasController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieManagementSystem.Data;
+
+namespace MovieManagementSystem.Controllers
+{
+    public class CinemasController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CinemasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var cinemas = await _context.Cinemas.ToListAsync();
+            return View();
+        }
+    }
+}
