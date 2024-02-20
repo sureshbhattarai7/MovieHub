8c7402997684a27372c0e851c6fea10501c8df07
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-02-20 20:47:41 +0545

    Add Index Controller of Actor

---
+++ MovieManagementSystem/Controllers/ActorsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieManagementSystem.Data;
+
+namespace MovieManagementSystem.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ActorsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var actors = await _context.Actors.ToListAsync();
+            return View();
+        }
+    }
+}
