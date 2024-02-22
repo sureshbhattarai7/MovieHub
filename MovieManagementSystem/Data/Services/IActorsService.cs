0062e07b72fc00c280e590c6bc0a7debfa2c1906
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-02-22 20:01:13 +0545

    Add Actors Interface

---
+++ MovieManagementSystem/Data/Services/IActorsService.cs
@@ -0,0 +1,13 @@
+﻿using MovieManagementSystem.Models;
+
+namespace MovieManagementSystem.Data.Services
+{
+    public interface IActorsService
+    {
+        Task<IEnumerable<Actor>> GetAll();
+        Actor GetById(int id);
+        void AddActor(Actor actor);
+        Actor Update (int id, Actor newActor);
+        void Delete(int id);
+    }
+}
