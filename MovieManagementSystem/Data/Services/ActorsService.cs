0408840907f7499ecbf832f621098b3b9962fcfb
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-02-22 20:01:52 +0545

    Add Actors Service

---
+++ MovieManagementSystem/Data/Services/ActorsService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieManagementSystem.Models;
+
+namespace MovieManagementSystem.Data.Services
+{
+    public class ActorsService : IActorsService
+    {
+        private readonly AppDbContext _context;
+        public ActorsService(AppDbContext context)
+        {
+
+            _context = context;
+
+        }
+        public void AddActor(Actor actor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Actor>> GetAll()
+        {
+            var result = await _context.Actors.ToListAsync(); 
+            return result;
+        }
+
+        public Actor GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Actor Update(int id, Actor newActor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
