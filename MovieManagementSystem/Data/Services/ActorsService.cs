fb44d16d3734e81e4ca05e363271f82431254b0d
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-02-23 22:43:45 +0545

    Implement GET method

--- MovieManagementSystem/Data/Services/ActorsService.cs
+++ MovieManagementSystem/Data/Services/ActorsService.cs
@@ -12,10 +12,10 @@ namespace MovieManagementSystem.Data.Services
             _context = context;
 
         }
-        public void AddActor(Actor actor)
+        public async Task AddActorAsync(Actor actor)
         {
-            _context.Actors.Add(actor);
-            _context.SaveChanges();
+            await _context.Actors.AddAsync(actor);
+            await _context.SaveChangesAsync();
         }
 
         public void Delete(int id)
@@ -23,20 +23,23 @@ namespace MovieManagementSystem.Data.Services
             throw new NotImplementedException();
         }
 
-        public async Task<IEnumerable<Actor>> GetAll()
+        public async Task<IEnumerable<Actor>> GetAllAsync()
         {
             var result = await _context.Actors.ToListAsync(); 
             return result;
         }
 
-        public Actor GetById(int id)
+        public async Task<Actor> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            var result = await _context.Actors.FirstOrDefaultAsync(n=>n.ActorId==id); 
+            return result;
         }
 
-        public Actor Update(int id, Actor newActor)
+        public async Task<Actor> UpdateAsync(int id, Actor newActor)
         {
-            throw new NotImplementedException();
+            _context.Update(newActor);
+            await _context.SaveChangesAsync();
+            return newActor;
         }
     }
 }
