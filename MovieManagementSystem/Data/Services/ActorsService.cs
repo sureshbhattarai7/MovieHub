7187caab2bc507da5ad52af972122052c94085db
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-02-24 20:21:04 +0545

    Add Delete Functionality

--- MovieManagementSystem/Data/Services/ActorsService.cs
+++ MovieManagementSystem/Data/Services/ActorsService.cs
@@ -18,9 +18,11 @@ namespace MovieManagementSystem.Data.Services
             await _context.SaveChangesAsync();
         }
 
-        public void Delete(int id)
+        public async Task DeleteAsync(int id)
         {
-            throw new NotImplementedException();
+            var result = await _context.Actors.FirstOrDefaultAsync(n => n.ActorId == id);
+            _context.Actors.Remove(result);
+            await _context.SaveChangesAsync();
         }
 
         public async Task<IEnumerable<Actor>> GetAllAsync()
