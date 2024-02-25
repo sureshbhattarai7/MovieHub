2bfa03b2e120bc281f80f5be163063855812ae28
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-02-25 21:48:21 +0545

    Inherit IEntityBaseRepository

--- MovieManagementSystem/Data/Services/IActorsService.cs
+++ MovieManagementSystem/Data/Services/IActorsService.cs
@@ -1,13 +1,10 @@
-﻿using MovieManagementSystem.Models;
+﻿using MovieManagementSystem.Data.Base;
+using MovieManagementSystem.Models;
 
 namespace MovieManagementSystem.Data.Services
 {
-    public interface IActorsService
+    public interface IActorsService:IEntityBaseRepository<Actor>
     {
-        Task<IEnumerable<Actor>> GetAllAsync();
-        Task<Actor> GetByIdAsync(int id);
-        Task AddActorAsync(Actor actor);
-        Task<Actor> UpdateAsync(int id, Actor newActor);
-        Task DeleteAsync(int id);
+       
     }
 }
