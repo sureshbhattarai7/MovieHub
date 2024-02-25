b0d0ff2477035b0396d63ecd36e68358b0337085
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-02-25 21:47:56 +0545

    Inherit EntityBaseRepository

--- MovieManagementSystem/Data/Services/ActorsService.cs
+++ MovieManagementSystem/Data/Services/ActorsService.cs
@@ -1,47 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MovieManagementSystem.Data.Base;
 using MovieManagementSystem.Models;
 
 namespace MovieManagementSystem.Data.Services
 {
-    public class ActorsService : IActorsService
+    public class ActorsService : EntityBaseRepository<Actor>, IActorsService
     {
-        private readonly AppDbContext _context;
-        public ActorsService(AppDbContext context)
-        {
-
-            _context = context;
-
-        }
-        public async Task AddActorAsync(Actor actor)
-        {
-            await _context.Actors.AddAsync(actor);
-            await _context.SaveChangesAsync();
-        }
-
-        public async Task DeleteAsync(int id)
-        {
-            var result = await _context.Actors.FirstOrDefaultAsync(n => n.ActorId == id);
-            _context.Actors.Remove(result);
-            await _context.SaveChangesAsync();
-        }
-
-        public async Task<IEnumerable<Actor>> GetAllAsync()
-        {
-            var result = await _context.Actors.ToListAsync(); 
-            return result;
-        }
-
-        public async Task<Actor> GetByIdAsync(int id)
-        {
-            var result = await _context.Actors.FirstOrDefaultAsync(n=>n.ActorId==id); 
-            return result;
-        }
-
-        public async Task<Actor> UpdateAsync(int id, Actor newActor)
-        {
-            _context.Update(newActor);
-            await _context.SaveChangesAsync();
-            return newActor;
-        }
+        public ActorsService(AppDbContext context) : base(context) { }
     }
+       
 }
