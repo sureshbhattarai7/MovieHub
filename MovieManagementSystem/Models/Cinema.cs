e9e473b48d575ee725307dd839e4d43680f35b49
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-02-28 21:22:25 +0545

    Inherit Entity base interface

--- MovieManagementSystem/Models/Cinema.cs
+++ MovieManagementSystem/Models/Cinema.cs
@@ -1,8 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using MovieManagementSystem.Data.Base;
+using System.ComponentModel.DataAnnotations;
 
 namespace MovieManagementSystem.Models
 {
-    public class Cinema
+    public class Cinema: IEntityBase
     {
         [Key]
         public int Id { get; set; }
