188bac9453e653d7159f295ee8735c0fa8d37612
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-03-02 20:04:56 +0545

    Implement Delete Cinema

--- MovieManagementSystem/Controllers/CinemasController.cs
+++ MovieManagementSystem/Controllers/CinemasController.cs
@@ -70,5 +70,25 @@ namespace MovieManagementSystem.Controllers
             }
             return View(cinema);
         }
+
+        //GET: Cinemas/Delete/ID
+        public async Task<IActionResult> Delete(int id)
+        {
+            var cinemas = await _service.GetByIdAsync(id);
+            if (cinemas == null) return View("NotFound");
+            return View(cinemas);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(id);
+            }
+
+            await _service.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
