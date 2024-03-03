d9f60f25b77b0bb588096c83a334aed592c33aec
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-03-03 17:42:44 +0545

    Add Movie Scope

--- MovieManagementSystem/Program.cs
+++ MovieManagementSystem/Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddScoped<AppDbInitializer>();
 builder.Services.AddScoped<IActorsService, ActorsService>();
 builder.Services.AddScoped<IProducersService, ProducersService>();
 builder.Services.AddScoped<ICinemasService, CinemasService>();
+builder.Services.AddScoped<IMoviesService, MoviesService>();
 
 var app = builder.Build();
 
