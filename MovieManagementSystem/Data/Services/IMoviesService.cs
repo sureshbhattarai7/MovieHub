357f153f80b3cde27e4db5ffab183e34a72d8e3a
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-03-04 15:20:00 +0545

    Implement GetMovieById functionality of movie

--- MovieManagementSystem/Data/Services/IMoviesService.cs
+++ MovieManagementSystem/Data/Services/IMoviesService.cs
@@ -5,5 +5,6 @@ namespace MovieManagementSystem.Data.Services
 {
     public interface IMoviesService:IEntityBaseRepository<Movie>
     {
+        Task<Movie> GetMovieByIdAsync(int id);
     }
 }
--- MovieManagementSystem/Data/Services/MoviesService.cs
+++ MovieManagementSystem/Data/Services/MoviesService.cs
@@ -1,13 +1,27 @@
-﻿using MovieManagementSystem.Data.Base;
+﻿using Microsoft.EntityFrameworkCore;
+using MovieManagementSystem.Data.Base;
 using MovieManagementSystem.Models;
 
 namespace MovieManagementSystem.Data.Services
 {
     public class MoviesService : EntityBaseRepository<Movie>, IMoviesService
     {
+        private readonly AppDbContext _context;
         public MoviesService(AppDbContext context): base(context)
         {
-            
+            _context = context;
+        }
+
+        public async Task<Movie> GetMovieByIdAsync(int id)
+        {
+            var movieDetails = await _context.Movies
+                .Include(c => c.Cinema)
+                .Include(p => p.Producer)
+                .Include(am => am.Actors_Movies)
+                .ThenInclude(a => a.Actor)
+                .FirstOrDefaultAsync(n => n.Id == id);
+
+            return movieDetails;
         }
     }
 }
