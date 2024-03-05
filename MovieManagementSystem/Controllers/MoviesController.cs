81da84738602f083210d21f59971b635b7ca41de
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-03-05 09:53:50 +0545

    Implement GetCreate

--- MovieManagementSystem/Controllers/MoviesController.cs
+++ MovieManagementSystem/Controllers/MoviesController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MovieManagementSystem.Data;
 using MovieManagementSystem.Data.Services;
+using System.Runtime.CompilerServices;
 
 namespace MovieManagementSystem.Controllers
 {
@@ -25,6 +27,21 @@ namespace MovieManagementSystem.Controllers
         {
             var movieDetail = await _service.GetByIdAsync(id);
             return View(movieDetail);
+        }
+
+        //GET: Movies/Create
+        public async Task<IActionResult> Create()
+        {
+            // ViewData["Welcome"] = "Welcome here";
+            // ViewBag.Description = "Welcome there";
+
+            var movieDropdownsData = await _service.GetNewMovieDropdownsValues();
+
+            ViewBag.Cinemas = new SelectList(movieDropdownsData.Cinemas, "Id", "CinemaName");
+            ViewBag.Producers = new SelectList(movieDropdownsData.Producers, "Id", "FullName");
+            ViewBag.Actors = new SelectList(movieDropdownsData.Actors, "Id", "FullName");
+
+            return View();
         }
     }
 }
