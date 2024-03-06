1bcdf70a35995a9db79233c5ac33c3c014c02ae3
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-03-06 17:14:55 +0545

    Implement Create Movie Functionality

--- MovieManagementSystem/Controllers/MoviesController.cs
+++ MovieManagementSystem/Controllers/MoviesController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MovieManagementSystem.Data;
 using MovieManagementSystem.Data.Services;
+using MovieManagementSystem.Models;
 using System.Runtime.CompilerServices;
 
 namespace MovieManagementSystem.Controllers
@@ -26,6 +27,8 @@ namespace MovieManagementSystem.Controllers
         public async Task<IActionResult> Details(int id)
         {
             var movieDetail = await _service.GetByIdAsync(id);
+            if (movieDetail == null) return View("NotFound");
+
             return View(movieDetail);
         }
 
@@ -42,6 +45,23 @@ namespace MovieManagementSystem.Controllers
             ViewBag.Actors = new SelectList(movieDropdownsData.Actors, "Id", "FullName");
 
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(NewMovieVM movie)
+        {
+            if(!ModelState.IsValid)
+            {
+                var movieDropdownsData = await _service.GetNewMovieDropdownsValues();
+
+                ViewBag.Cinemas = new SelectList(movieDropdownsData.Cinemas, "Id", "CinemaName");
+                ViewBag.Producers = new SelectList(movieDropdownsData.Producers, "Id", "FullName");
+                ViewBag.Actors = new SelectList(movieDropdownsData.Actors, "Id", "FullName");
+                return View(movie);
+            }
+
+            await _service.AddNewMovieAsync(movie);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
