229ce29351654d39aaafc7956eab0990d94a5f6d
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-03-07 15:21:07 +0545

    Add ShoppingCartItem Model

---
+++ MovieManagementSystem/Models/ShoppingCartItem.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieManagementSystem.Models
+{
+    public class ShoppingCartItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public Movie Movie { get; set; }
+
+        public int Amount { get; set; }
+        public int ShoppingCartId { get; set; }
+    }
+}
