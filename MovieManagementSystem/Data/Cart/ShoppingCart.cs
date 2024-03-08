e79e579ae19442d7348a2bc65f57239163b05461
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-03-08 16:35:18 +0545

    Setting up ShoppingCart class

---
+++ MovieManagementSystem/Data/Cart/ShoppingCart.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieManagementSystem.Models;
+
+namespace MovieManagementSystem.Data.Cart
+{
+    public class ShoppingCart
+    {
+        public AppDbContext _context { get; set; }
+        public int ShoppingCartId { get; set; }
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+
+        public ShoppingCart(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<ShoppingCartItem> GetShoppingCartItems()
+        {
+            return ShoppingCartItems ?? (ShoppingCartItems = _context.ShoppingCartItems.Where(n => n.ShoppingCartId == ShoppingCartId).Include(n => n.Movie).ToList());
+        }
+
+        public double GetShoppingCartTotal() => _context.ShoppingCartItems.Where(n => n.ShoppingCartId == ShoppingCartId).Select(n => n.Movie.Price * n.Amount).Sum();
+
+    }
+}
