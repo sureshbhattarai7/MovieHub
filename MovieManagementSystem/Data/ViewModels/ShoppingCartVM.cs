f0d5fc33cec68294be5b6452900ea05d1e553a8e
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-03-10 11:31:54 +0545

    Add shopping cart view model

---
+++ MovieManagementSystem/Data/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,10 @@
+﻿using MovieManagementSystem.Data.Cart;
+
+namespace MovieManagementSystem.Data.ViewModels
+{
+    public class ShoppingCartVM
+    {
+        public ShoppingCart ShoppingCart { get; set; }
+        public double ShoppingCartTotal { get; set; }
+    }
+}
