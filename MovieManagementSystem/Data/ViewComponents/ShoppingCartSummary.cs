296493737f75dcb19e9b6b44b59179ded72c3258
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-03-15 19:13:36 +0545

    Add ShoppingCartItem Action

---
+++ MovieManagementSystem/Data/ViewComponents/ShoppingCartSummary.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using MovieManagementSystem.Data.Cart;
+
+namespace MovieManagementSystem.Data.ViewComponents
+{
+    public class ShoppingCartSummary:ViewComponent
+    {
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+
+            return View(items.Count);
+        }
+    }
+}
