22faa39e09da0d29c5f87fba6c12c60d2139604c
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-03-16 20:23:41 +0545

    Adding and Configuring OrdersService

---
+++ MovieManagementSystem/Data/Services/IOrdersService.cs
@@ -0,0 +1,11 @@
+﻿using MovieManagementSystem.Models;
+
+namespace MovieManagementSystem.Data.Services
+{
+    public interface IOrdersService
+    {
+        Task StoreOrderAsync(List<ShoppingCartItem> items, string userId, string userEmail);
+
+        Task<List<Order>> GetOrdersByUserIdAsync(string userId);
+    }
+}
---
+++ MovieManagementSystem/Data/Services/OrdersService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieManagementSystem.Models;
+
+namespace MovieManagementSystem.Data.Services
+{
+    public class OrdersService : IOrdersService
+    {
+        private readonly AppDbContext _context;
+
+        public OrdersService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Order>> GetOrdersByUserIdAsync(string userId)
+        {
+            var orders = await _context.Orders.Include(n => n.OrderItems).ThenInclude( n => n.Movie).Where(n => n.UserId == userId ).ToListAsync();
+            return orders;
+        }
+
+        public async Task StoreOrderAsync(List<ShoppingCartItem> items, string userId, string userEmail)
+        {
+            var order = new Order()
+            {
+                UserId = userId,
+                Email = userEmail
+            };
+            await _context.Orders.AddAsync(order);
+            await _context.SaveChangesAsync();
+
+            foreach(var item in items)
+            {
+                var orderItem = new OrderItem()
+                {
+                    Amount = item.Amount,
+                    MovieId = item.Movie.Id,
+                    OrderId = order.Id,
+                    Price = item.Movie.Price
+                };
+
+                await _context.OrderItems.AddAsync(orderItem);
+            }
+            await _context.SaveChangesAsync();
+        }
+    }
+}
