b9355af87279733732b46dfe0d1b030a2cdd17cb
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-03-17 17:37:03 +0545

    Created ClearShoppingCart functionality after completing the order

--- MovieManagementSystem/Data/Cart/ShoppingCart.cs
+++ MovieManagementSystem/Data/Cart/ShoppingCart.cs
@@ -73,6 +73,12 @@ namespace MovieManagementSystem.Data.Cart
 
         public double GetShoppingCartTotal() => _context.ShoppingCartItems.Where(n => n.ShoppingCartId == ShoppingCartId).Select(n => n.Movie.Price * n.Amount).Sum();
 
+        public async Task ClearShoppingCartAsync()
+        {
+            var items = await _context.ShoppingCartItems.Where(n => n.ShoppingCartId == ShoppingCartId).ToListAsync();
+            _context.ShoppingCartItems.RemoveRange(items);
+            await _context.SaveChangesAsync();
+        }
     }
 }
 
