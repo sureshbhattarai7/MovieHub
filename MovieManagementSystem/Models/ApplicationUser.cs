06dd24f6bc88d9ca0fc120e7bf41771775ac9298
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-03-19 20:03:51 +0545

    Add ApplicationUser Model

---
+++ MovieManagementSystem/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieManagementSystem.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Display(Name = "Full Name")]
+        public required string FullName { get; set; }
+    }
+}
