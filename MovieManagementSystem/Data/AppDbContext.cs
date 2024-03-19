28d6f8de55e7e8250743ff584d3fb7658c4f128f
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-03-19 20:03:15 +0545

    Inherit IdentityDbContext in AppDbContext

--- MovieManagementSystem/Data/AppDbContext.cs
+++ MovieManagementSystem/Data/AppDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using MovieManagementSystem.Models;
 
 namespace MovieManagementSystem.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
         {
