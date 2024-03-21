51641de2eb912e85977c4e926ac372dfce7a0fba
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-03-21 19:55:08 +0545

    Identity Configuration in Startup.cs

--- MovieManagementSystem/Program.cs
+++ MovieManagementSystem/Program.cs
@@ -3,6 +3,9 @@ using MovieManagementSystem.Data;
 using MovieManagementSystem.Data.Cart;
 using MovieManagementSystem.Data.Services;
 using Microsoft.AspNetCore.Http;
+using MovieManagementSystem.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,7 +26,14 @@ builder.Services.AddScoped<IOrdersService, OrdersService>();
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 builder.Services.AddScoped(sc => ShoppingCart.GetShoppingCart(sc));
 
+//Authentication and Authorization
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+builder.Services.AddMemoryCache();
 builder.Services.AddSession();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+});
 
 var app = builder.Build();
 
@@ -42,6 +52,8 @@ app.UseStaticFiles();
 app.UseRouting();
 app.UseSession();
 
+//Authentication and Authorization
+app.UseAuthentication();
 app.UseAuthorization();
 
 //Seed Database
