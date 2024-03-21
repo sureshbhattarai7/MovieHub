310bbd0014ee47f2926dea21d9373991195b12ae
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-03-21 19:59:03 +0545

    Adding AccountController

---
+++ MovieManagementSystem/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MovieManagementSystem.Data;
+using MovieManagementSystem.Models;
+
+namespace MovieManagementSystem.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly AppDbContext _context;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, AppDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
