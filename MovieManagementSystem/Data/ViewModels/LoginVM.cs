98c5ea087d1d22e78b9dbff6e62fb44723091d43
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-03-22 20:18:44 +0545

    Add Login View Model

---
+++ MovieManagementSystem/Data/ViewModels/LoginVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieManagementSystem.Data.ViewModels
+{
+    public class LoginVM
+    {
+        [Required(ErrorMessage = "Email Address is required!")]
+        [Display(Name = "Email Address")]
+        public string? EmailAddress { get; set; }
+
+        [Required(ErrorMessage = "Password is required!")]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+    }
+}
