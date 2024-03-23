882f976b5340697be66c9ac448e4d4b701611dcd
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-03-23 20:31:53 +0545

    Implemented Login Action

--- MovieManagementSystem/Controllers/AccountController.cs
+++ MovieManagementSystem/Controllers/AccountController.cs
@@ -19,5 +19,30 @@ namespace MovieManagementSystem.Controllers
             _context = context;
         }
         public IActionResult Login() => View(new LoginVM());
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM loginVM)
+        {
+            if (!ModelState.IsValid) return View(loginVM);
+
+            var user = await _userManager.FindByEmailAsync(loginVM.EmailAddress);
+            if(user != null)
+            {
+                var passwordCheck = await _userManager.CheckPasswordAsync(user, loginVM.Password);
+                if (passwordCheck)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Movies");
+                    }
+                }
+                TempData["Error"] = "Wrong Credentials. Please try again!";
+                return View(loginVM);
+            }
+
+            TempData["Error"] = "Wrong Credentials. Please try again!";
+            return View(loginVM);
+        }
     }
 }
