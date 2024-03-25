bd48ec203b75ed842bf09923ecd965981ce90e1f
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-03-25 20:55:39 +0545

    Add Signup ViewModel

---
+++ MovieManagementSystem/Data/ViewModels/SignupVM.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieManagementSystem.Data.ViewModels
+{
+    public class SignupVM
+    {
+        [Required(ErrorMessage = "FullName is required!")]
+        [Display(Name = "Full Name")]
+        public string? FullName { get; set; }
+
+        [Required(ErrorMessage = "Email Address is required!")]
+        [Display(Name = "Email Address")]
+        public string? EmailAddress { get; set; }
+
+        [Required(ErrorMessage = "Password is required!")]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        [Display(Name ="Confirm Password")]
+        [Required(ErrorMessage = "Confirm Password is required!")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage =("Passwords do not match"))]
+        public string? ConfirmPassword { get; set; }
+    }
+}
