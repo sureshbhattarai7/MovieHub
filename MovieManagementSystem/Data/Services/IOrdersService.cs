d8ce12846566c03948a123c44507eb0436be832e
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-03-26 21:59:13 +0545

    Implement Role based order

--- MovieManagementSystem/Data/Services/IOrdersService.cs
+++ MovieManagementSystem/Data/Services/IOrdersService.cs
@@ -6,6 +6,6 @@ namespace MovieManagementSystem.Data.Services
     {
         Task StoreOrderAsync(List<ShoppingCartItem> items, string userId, string userEmail);
 
-        Task<List<Order>> GetOrdersByUserIdAsync(string userId);
+        Task<List<Order>> GetOrdersByUserIdAndRoleAsync(string userId, string userRole);
     }
 }
--- MovieManagementSystem/Data/Services/OrdersService.cs
+++ MovieManagementSystem/Data/Services/OrdersService.cs
@@ -11,9 +11,13 @@ namespace MovieManagementSystem.Data.Services
         {
             _context = context;
         }
-        public async Task<List<Order>> GetOrdersByUserIdAsync(string userId)
+        public async Task<List<Order>> GetOrdersByUserIdAndRoleAsync(string userId, string userRole)
         {
-            var orders = await _context.Orders.Include(n => n.OrderItems).ThenInclude( n => n.Movie).Where(n => n.UserId == userId ).ToListAsync();
+            var orders = await _context.Orders.Include(n => n.OrderItems).ThenInclude( n => n.Movie).ToListAsync();
+            if (userRole != "Admin")
+            {
+                orders = orders.Where(n => n.UserId == userId).ToList();
+            }
             return orders;
         }
 
